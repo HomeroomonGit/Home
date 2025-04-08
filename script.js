024aa72415432369b7a805b00b4f8cf37d357447
HomeroomonGit <r6tipsr6s2@gmail.com>
2025-04-08 19:12:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.getElementById('clickButton').addEventListener('click', function() {
+    alert('Button clicked!');
+});
+
+document.getElementById('cloakButton').addEventListener('click', function() {
+    // Change the tab title when the cloak button is clicked
+    document.title = "Cloaked Tab"; // You can set this to any name you'd like
+});
 
